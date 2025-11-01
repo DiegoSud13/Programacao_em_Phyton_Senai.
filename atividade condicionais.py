3dd48035203e100f1a7e98c866676e1181298b0e
DiegoSud13 <dcelestinosouzaramos@gmail.com>
2025-11-01 16:14:32 -0300

    Add files via upload

---
+++ atividade condicionais.py
@@ -0,0 +1,55 @@
+
+numero = float(input('digite um numero'))
+
+if numero>0:
+    print('o numero é positivo')
+elif numero <0:
+    print('o numero é negativo')
+else:  
+    print('o numero é zero')
+
+
+
+idade = int(input('digite um numero'))
+if idade >=18:
+    print('voce pode votar')
+else:
+    print('voce ainda nao pode votar')
+
+
+
+if numero % 2==0:
+    print('o numero é par')
+else:
+    print(' o numero é impar')
+
+
+
+numero = int(input('digite um numero'))
+if numero % 5 == 0 and numero % 7 ==0: 
+    print(' o numero é mutiplo de 5 e de 7')
+elif numero % 5 ==0:
+    print (' o numero é mutipplo de 5')
+elif numero % 7 ==0: 
+     print (' o numero é mutipplo de 7')
+else: print('o numero nao é mutiplo de 5 nem de 7')
+
+
+ 
+if numero > 0 and numero > 10:
+    print('o numero é positivo e maior que 10')
+elif numero >0 and numero <=10:
+    print(' o numero é positivo, mas nao é maior que 10')
+else: 
+    print('o numero nao é positivo')
+
+
+
+numero = int(input('digite um numero'))
+if numero %3==0 or numero % 5==0: 
+    print(' o numero é divisel por 3 ou por 5')
+else: 
+    print(' o numero nao é divisivel por 3 nem por 5')
+
+
+
